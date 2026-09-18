03c0097fac05bc39389ad1bd45d342afe3d29693
Sara <Sara.Rezaii@Student.HTW-Berlin.de>
2026-09-18 19:03:05 +0200

    test landing page buttons

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -13,11 +13,35 @@ describe('App', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
   it('should render title', async () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-expect(compiled.querySelector('h1')?.textContent).toContain('MyWishlist');
+    expect(compiled.querySelector('h1')?.textContent).toContain('MyWishlist');
   });
+  it('should open the add wish form', () => {
+    const fixture = TestBed.createComponent(App);
+    fixture.detectChanges();
+
+    const compiled = fixture.nativeElement as HTMLElement;
+    const addButton = compiled.querySelector('.add-button') as HTMLButtonElement;
+
+    addButton.click();
+    fixture.detectChanges();
+
+    expect(compiled.querySelector('#wish-name')).toBeTruthy();
+  });
+  it('should open the wishlist', () => {
+    const fixture = TestBed.createComponent(App);
+    fixture.detectChanges();
+
+    const compiled = fixture.nativeElement as HTMLElement;
+    const listButton = compiled.querySelector('.list-button') as HTMLButtonElement;
+
+    listButton.click();
+    fixture.detectChanges();
+
+    expect(compiled.querySelector('.list-group')).toBeTruthy();
+  });
+
 });
